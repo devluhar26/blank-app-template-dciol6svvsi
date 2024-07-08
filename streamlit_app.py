5e5aaf41222970543c1a544f70d74f93c313562d
dev <devluhar26@gmail.com>
2024-07-08 12:00:07 +0100

    ,

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,24 +74,7 @@ for i in sys.argv[1:]:
 # construct component props dictionary (->Code Editor)
 comp_props = {"css": css_text, "globalCSS": ":root {\n  --streamlit-dark-font-family: monospace;\n}"}
 
-mode_list = ["abap", "abc", "actionscript", "ada", "alda", "apache_conf", "apex", "applescript", "aql", "asciidoc",
-             "asl", "assembly_x86", "autohotkey", "batchfile", "bibtex", "c9search", "c_cpp", "cirru", "clojure",
-             "cobol", "coffee", "coldfusion", "crystal", "csharp", "csound_document", "csound_orchestra",
-             "csound_score", "csp", "css", "curly", "d", "dart", "diff", "django", "dockerfile", "dot", "drools",
-             "edifact", "eiffel", "ejs", "elixir", "elm", "erlang", "forth", "fortran", "fsharp", "fsl", "ftl", "gcode",
-             "gherkin", "gitignore", "glsl", "gobstones", "golang", "graphqlschema", "groovy", "haml", "handlebars",
-             "haskell", "haskell_cabal", "haxe", "hjson", "html", "html_elixir", "html_ruby", "ini", "io", "ion",
-             "jack", "jade", "java", "javascript", "jexl", "json", "json5", "jsoniq", "jsp", "jssm", "jsx", "julia",
-             "kotlin", "latex", "latte", "less", "liquid", "lisp", "livescript", "logiql", "logtalk", "lsl", "lua",
-             "luapage", "lucene", "makefile", "markdown", "mask", "matlab", "maze", "mediawiki", "mel", "mips", "mixal",
-             "mushcode", "mysql", "nginx", "nim", "nix", "nsis", "nunjucks", "objectivec", "ocaml", "partiql", "pascal",
-             "perl", "pgsql", "php", "php_laravel_blade", "pig", "plain_text", "powershell", "praat", "prisma",
-             "prolog", "properties", "protobuf", "puppet", "python", "qml", "r", "raku", "razor", "rdoc", "red",
-             "redshift", "rhtml", "robot", "rst", "ruby", "rust", "sac", "sass", "scad", "scala", "scheme", "scrypt",
-             "scss", "sh", "sjs", "slim", "smarty", "smithy", "snippets", "soy_template", "space", "sparql", "sql",
-             "sqlserver", "stylus", "svg", "swift", "tcl", "terraform", "tex", "text", "textile", "toml", "tsx",
-             "turtle", "twig", "typescript", "vala", "vbscript", "velocity", "verilog", "vhdl", "visualforce", "wollok",
-             "xml", "xquery", "yaml", "zeek"]
+
 
 btn_settings_editor_btns = [{
     "name": "copy",
@@ -125,16 +108,6 @@ with col1:
     ace_props = {"style": {"borderRadius": "0px 0px 8px 8px"}}
     response_dict = code_editor(demo_sample_python_code, height=height, lang=language, theme=theme, shortcuts=shortcuts,
                                 focus=focus, buttons=btns, info=info_bar, props=ace_props)
-
-    if response_dict['type'] == "submit" and len(response_dict['text']) != 0:
-        st.write("Response type: ", response_dict['type'])
-        st.code(response_dict['text'], language=response_dict['lang'])
-
-    st.markdown("## Disable line wrapping")
-    linewrap_demo_code = """# change editor (session) option 'wrap' to False to disable line wrapping
-response_dict = code_editor(linewrap_demo_code,lang="python", height = [2, 4], options={"wrap": False})"""
-
-    response_dict = code_editor(linewrap_demo_code, lang="python", height=[2, 4], options={"wrap": False})
 
     if response_dict['type'] == "submit" and len(response_dict['text']) != 0:
         st.write("Response type: ", response_dict['type'])
